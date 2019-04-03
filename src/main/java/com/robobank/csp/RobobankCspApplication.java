3e979423e752a6f493fd57c4343d5b5b2a5a7513
Karthikeyan Nithiyanandam <39563373+karthikeyan-ng@users.noreply.github.com>
2019-04-03 18:50:47 +0530

    Initial Project structure

---
+++ src/main/java/com/robobank/csp/RobobankCspApplication.java
@@ -0,0 +1,13 @@
+package com.robobank.csp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobobankCspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobobankCspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robobank/csp/RobobankCspApplicationTests.java
@@ -0,0 +1,16 @@
+package com.robobank.csp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RobobankCspApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
